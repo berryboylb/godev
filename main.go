42e3f663217015dc4275e474a97c4ba3b76a5dde
berryboylb <phemmynesce4life@gmail.com>
2024-01-12 22:09:19 +0100

    @chore: buff

--- main.go
+++ main.go
@@ -186,6 +186,22 @@ func isPalindrome(s string) bool {
 	return true
 }
 
+func singleNumber(nums []int) int {
+	items := make(map[int]int)
+	for _, value := range nums {
+		if _, ok := items[value]; !ok {
+			items[value] = value
+		} else {
+			delete(items, value)
+		}
+	}
+	for key, _ := range items {
+		return key
+	}
+
+	return 0
+}
+
 func main() {
 	// twoSum([]int{2,7,11,15}, 9)
 	// removeDuplicates([]int{0,0,1,1,1,2,2,3,3,4})
@@ -194,5 +210,6 @@ func main() {
 	// searchInsert([]int{1,3,5,6}, 7)
 	// lengthOfLastWord("   fly me   to   the moon  ")
 	// plusOne([]int{7, 2, 8, 5, 0, 9, 1, 2, 9, 5, 3, 6, 6, 7, 3, 2, 8, 4, 3, 7, 9, 5, 7, 7, 4, 7, 4, 9, 4, 7, 0, 1, 1, 1, 7, 4, 0, 0, 6})
-	isPalindrome("0P")
+	// isPalindrome("0P")
+	singleNumber([]int{4,1,2,1,2})
 }
