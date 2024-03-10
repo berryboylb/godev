06d5fbad31b487c05414f0969b3144205efc31ea
berryboylb <phemmynesce4life@gmail.com>
2024-03-10 17:00:55 +0100

    @chore: solution

--- main.go
+++ main.go
@@ -958,6 +958,37 @@ func mostCommonWord(paragraph string, banned []string) string {
 	return ans
 }
 
+
+func intToRoman(num int) string {
+    res := ""
+	numerals := map[int]string{
+		1: "I",
+		4: "IV",
+		5: "V",
+		9: "IX",
+		10: "X",
+		40: "XL",
+		50: "L",
+		90: "XC",
+		100: "C",
+		400: "CD",
+		500: "D",
+		900: "CM",
+		1000: "M",
+	}
+	keys := []int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
+
+	for _, key := range keys {
+		for num >= key {
+			res += numerals[key]
+			num -= key
+		}
+	}
+	fmt.Println(res)
+
+	return res
+}
+
 func main() {
 	// twoSum([]int{2,7,11,15}, 9)
 	// removeDuplicates([]int{0,0,1,1,1,2,2,3,3,4})
@@ -1005,5 +1036,6 @@ func main() {
 	// maxProduct([]int{-2, 0, -1})
 	// selfDividingNumbers(1, 22)
 	// maxProfit2([]int{7,1,5,3,6,4})
-	mostCommonWord("Bob hit a ball, the hit BALL flew far after it was hit.", []string{"hit"})
+	// mostCommonWord("Bob hit a ball, the hit BALL flew far after it was hit.", []string{"hit"})
+	intToRoman(5)
 }
